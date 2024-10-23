eef12a805f958d85e7a96b3d8bfa59ee4eea8a7d
JH <tarakaing@naver.com>
2024-10-23 10:51:54 +0900

    첫번째 커밋

---
+++ src/main/java/com/group/libraryapp/dto/book/request/BookReturnRequest.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.dto.book.request;
+
+public class BookReturnRequest {
+
+    private String UserName;
+    private String bookName;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+}
